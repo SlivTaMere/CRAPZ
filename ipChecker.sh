038d2eac3ab6d107a164b8d634b8588351603b6c
SlivTaMere <slivtamere@gmail.com>
2016-08-02 17:18:18 -0400

    Update ipChecker.sh

--- ipChecker.sh
+++ ipChecker.sh
@@ -6,5 +6,5 @@ ip=$(curl -s -k https://api.ipify.org)
 if [ $(cat $ipFile) != $ip ]
 then
 	echo -n "$ip" > $ipFile
-	sendemail -q -f FROMemail -t TOemail -m "new IP: $ip" -s smtp.gmail.com:587 -xu gmailUserName -xp gmailPassword -o tls=yes -o
+	sendemail -q -f FROMemail -t TOemail -m "new IP: $ip" -s smtp.gmail.com:587 -xu gmailUserName -xp gmailPassword -o tls=yes
 fi
